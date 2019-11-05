53f18cb0418eba6c5f4bbc309890b02530e5b6ad
SebaD95 <sebadiaz95@hotmail.com>
2019-11-05 17:40:43 -0300

    a

---
+++ Shared/WebLayer/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WEBLayer.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Envios()
+        {
+            return View();
+        }
+
+        public ActionResult Perfil()
+        {
+            return View();
+        }
+
+        public ActionResult SeguirPaquete()
+        {
+            return View();
+        }
+
+        public ActionResult Recibidos()
+        {
+            return View();
+        }
+    }
+}
