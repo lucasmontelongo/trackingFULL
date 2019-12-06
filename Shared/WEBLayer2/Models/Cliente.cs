4f270d555697eee73520d65609c71c70eb3e2c2c
SebaD95 <sebadiaz95@hotmail.com>
2019-12-06 16:18:06 -0300

    vistas admin tablas

--- Shared/WEBLayer2/Models/Cliente.cs
+++ Shared/WEBLayer2/Models/Cliente.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace WEBLayer2.Models
 {
@@ -9,9 +11,14 @@ namespace WEBLayer2.Models
     {
         public int Id { get; set; }
         public string Email { get; set; }
+        [Display(Name = "Nombre Completo")]
         public string NombreCompleto { get; set; }
+        [Display(Name = "Teléfono")]
+
         public string Telefono { get; set; }
+        [Display(Name = "Documento")]
         public string NumeroDocumento { get; set; }
+        [Display(Name = "Tipo de Documento")]
         public string TipoDocumento { get; set; }
         public bool Borrado { get; set; }
     }
---
+++ Shared/WEBLayer2/Scripts/pagination.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    
+    $('#clientes').DataTable({
+        language: {
+            "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
+        },
+        pageLength: 5,
+        lengthMenu: [[5, 10, 20, -1], [5, 10, 20, 'Todos']], 
+    });
+});
+
